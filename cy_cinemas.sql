f8136b0d69c2cfb021605954b412aebf03e30b9f
Louis3240 <rt4827488@yahoo.com.tw>
2019-09-27 15:36:56 +0800

    update more tables point,wallet,ticket_set and rename some typing style

--- cy_cinemas.sql
+++ cy_cinemas.sql
@@ -12,165 +12,189 @@ USE `cy_cinemas`;
 -- 最新消息
 DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news` (
-    `news_id` INT AUTO_INCREMENT NOT NUll,
-    `news_title` VARCHAR(25) NOT NUll,
-    `news_content` TEXT NOT NUll,
-    `news_img_normal_url` VARCHAR(255) NOT NUll,
-    `news_img_thumbs_url` VARCHAR(255) NOT NUll,
-    `news_time` TIMESTAMP NOT NUll,
-    PRIMARY KEY (`news_id`)
+    `id` INT AUTO_INCREMENT NOT NUll,
+    `title` VARCHAR(25) NOT NUll,
+    `content` TEXT NOT NUll,
+    `img_normal_url` VARCHAR(255) NOT NUll,
+    `img_thumbs_url` VARCHAR(255) NOT NUll,
+    `time` TIMESTAMP NOT NUll,
+    PRIMARY KEY (`id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 會員
 DROP TABLE IF EXISTS `members`;
 CREATE TABLE `members` (
-    `members_id` INT AUTO_INCREMENT NOT NUll,
-    `members_name` VARCHAR(20) NOT NUll,
-    `members_account` VARCHAR(20) NOT NUll,
-    `members_password` VARCHAR(15) NOT NUll,
-    `members_email` VARCHAR(50) NOT NUll,
-    `members_phone` VARCHAR(10) NOT NUll,
-    `members_wallet` TINYINT DEFAULT 0,
-    `members_point`  TINYINT DEFAULT 0,
-    PRIMARY KEY (`members_id`),
-    UNIQUE KEY (`members_account`)
+    `id` INT AUTO_INCREMENT NOT NUll,
+    `name` VARCHAR(20) NOT NUll,
+    `account` VARCHAR(20) NOT NUll,
+    `password` VARCHAR(100) NOT NUll,
+    `email` VARCHAR(100) NOT NUll,
+    `phone` VARCHAR(10) NOT NUll,
+    `wallet` TINYINT DEFAULT 0,
+    `point`  TINYINT DEFAULT 0,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`account`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 餐點
 DROP TABLE IF EXISTS `food_drinks`;
 CREATE TABLE `food_drinks` (
-    `food_drinks_id` INT AUTO_INCREMENT NOT NUll,
-    `food_drinks_name` VARCHAR(20) NOT NULL,
-    `food_drinks_size` VARCHAR(5) NOT NULL,
-    `food_drinks_price` INT NOT NULL,
-    PRIMARY KEY (`food_drinks_id`)
+    `id` INT AUTO_INCREMENT NOT NUll,
+    `name` VARCHAR(20) NOT NULL,
+    `size` VARCHAR(5) NOT NULL,
+    `price` INT NOT NULL,
+    PRIMARY KEY (`id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 正在上映的電影
 DROP TABLE IF EXISTS `movies`;
 CREATE TABLE `movies`
 (
-  `movies_id` INT AUTO_INCREMENT NOT NUll,
-  `movies_encoded_id` VARCHAR(50),
-  `movies_name` VARCHAR(20),
-  `movies_enname` VARCHAR(30),
-  `movies_rating` VARCHAR(20),
-  `movies_run_time` VARCHAR(10),
-  `movies_info` TEXT,
-  `movies_actor` VARCHAR(20),
-  `movies_genre` VARCHAR(10),
-  `movies_play_date` VARCHAR(20),
-  `movies_poster` TEXT,
-  `movies_trailer` TEXT,
-  PRIMARY KEY (`movies_id`),
-  UNIQUE KEY (`movies_encoded_id`)
+  `id` INT AUTO_INCREMENT NOT NUll,
+  `encoded_id` VARCHAR(50),
+  `name` VARCHAR(20),
+  `enname` VARCHAR(30),
+  `rating` VARCHAR(20),
+  `run_time` VARCHAR(10),
+  `info` TEXT,
+  `actor` VARCHAR(20),
+  `genre` VARCHAR(10),
+  `play_date` VARCHAR(20),
+  `poster` TEXT,
+  `trailer` TEXT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`encoded_id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- 即將上映電影
-DROP TABLE IF EXISTS `coming_movies`;
-CREATE TABLE `coming_movies`
-(
-  `coming_movies_id` INT AUTO_INCREMENT NOT NUll,
-  `coming_movies_encoded_id` VARCHAR(50),
-  `coming_movies_name` VARCHAR(20),
-  `coming_movies_enname` VARCHAR(30),
-  `coming_movies_info` TEXT,
-  `coming_movies_actor` VARCHAR(20),
-  `coming_movies_genre` VARCHAR(10),
-  `coming_movies_play_date` VARCHAR(20),
-  `coming_movies_poster` TEXT,
-  `coming_movies_trailer` TEXT,
-  PRIMARY KEY (`coming_movies_id`),
-  UNIQUE KEY (`coming_movies_encoded_id`)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 影城資訊
 DROP TABLE IF EXISTS `theaters`;
 CREATE TABLE `theaters`
 (
- `theaters_id` INT AUTO_INCREMENT NOT NULL,
- `theaters_name` VARCHAR(20),
- `theaters_address` VARCHAR(30),
- `theaters_phone` VARCHAR(20),
- `theaters_imgurl` TEXT,
- PRIMARY KEY (`theaters_id`)
+ `id` INT AUTO_INCREMENT NOT NULL,
+ `name` VARCHAR(20),
+ `address` VARCHAR(30),
+ `phone` VARCHAR(20),
+ `imgurl` TEXT,
+ PRIMARY KEY (`id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 電影時刻
 DROP TABLE IF EXISTS `movie_time`;
 CREATE TABLE `movie_time`
 (
-  `movie_time_id` INT AUTO_INCREMENT NOT NULL,
+  `id` INT AUTO_INCREMENT NOT NULL,
   `movies_encoded_id` VARCHAR(50),
   `theaters_name` VARCHAR(20),
-  `movie_time_seat_tag` VARCHAR(20),
-  `movie_time_time` VARCHAR(10),
-  `movie_time_seat_info` VARCHAR(10),
-  PRIMARY KEY (`movie_time_id`),
-  FOREIGN KEY(`movies_encoded_id`) REFERENCES `movies`(`movies_encoded_id`)
+  `seat_tag` VARCHAR(20),
+  `time` VARCHAR(10),
+  `seat_info` VARCHAR(10),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`movies_encoded_id`) REFERENCES `movies`(`encoded_id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 電影日期
 DROP TABLE IF EXISTS `movie_day`;
 CREATE TABLE `movie_day`
 (
-  `movie_day_id` INT AUTO_INCREMENT NOT NULL,
+  `id` INT AUTO_INCREMENT NOT NULL,
   `movies_encoded_id` VARCHAR(50),
-  `movie_day_weekday` VARCHAR(20),
-  `movie_day_date` DATE,
-  PRIMARY KEY (`movie_day_id`),
-  FOREIGN KEY(`movies_encoded_id`) REFERENCES `movies`(`movies_encoded_id`)
+  `weekday` VARCHAR(20),
+  `date` DATE,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`movies_encoded_id`) REFERENCES `movies`(`encoded_id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 廳
 DROP TABLE IF EXISTS `courts`;
 CREATE TABLE `courts`
 (
-  `courts_id` INT AUTO_INCREMENT NOT NULL,
-  `courts_seats` VARCHAR(50),
-  `courts_name` varchar(10),
-  PRIMARY KEY (`courts_id`)
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `seats` VARCHAR(50),
+  `name` varchar(10),
+  PRIMARY KEY (`id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 放映場次
 DROP TABLE IF EXISTS `screenings`;
 CREATE TABLE `screenings`
 (
-  `screenings_id` INT AUTO_INCREMENT NOT NULL,
+  `id` INT AUTO_INCREMENT NOT NULL,
   `movies_encoded_id` VARCHAR(50),
   `movie_time_time` VARCHAR(20),
   `movie_day_date` DATE,
   `courts_id` INT,
-  PRIMARY KEY (`screenings_id`),
-  FOREIGN KEY(`movies_encoded_id`) REFERENCES `movies`(`movies_encoded_id`),
-  FOREIGN KEY(`courts_id`) REFERENCES `courts`(`courts_id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`movies_encoded_id`) REFERENCES `movies`(`encoded_id`),
+  FOREIGN KEY(`courts_id`) REFERENCES `courts`(`id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 票種
 DROP TABLE IF EXISTS `tickets`;
 CREATE TABLE `tickets`
 (
-  `tickets_id` INT AUTO_INCREMENT NOT NULL,
-  `tickets_name` VARCHAR(50),
-  `tickets_price` INT,
-  PRIMARY KEY (`tickets_id`)
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `name` VARCHAR(50),
+  `price` INT,
+  PRIMARY KEY (`id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 訂單
 DROP TABLE IF EXISTS `order_details`;
 CREATE TABLE `order_details`
 (
-  `order_details_id` INT AUTO_INCREMENT NOT NULL,
+  `id` INT AUTO_INCREMENT NOT NULL,
   `screenings_id` INT,
   `members_id` INT,
-  `order_details_num` INT,
-  `order_details_seat` VARCHAR(50),
-  `order_details_total_price` INT,
-  `order_details_discounted_price` INT,
-  `order_details_tickets_num` VARCHAR(30),
-  `order_details_food_drinks_num` VARCHAR(30),
-  `order_details_datetime`  TIMESTAMP,
-  PRIMARY KEY (`order_details_id`),
-  FOREIGN KEY(`screenings_id`) REFERENCES `screenings`(`screenings_id`),
-  FOREIGN KEY(`members_id`) REFERENCES `members`(`members_id`)
+  `num` INT,
+  `seat` VARCHAR(50),
+  `total_price` INT,
+  `discounted_price` INT,
+  `tickets_num` VARCHAR(30),
+  `food_drinks_num` VARCHAR(30),
+  `datetime`  TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`screenings_id`) REFERENCES `screenings`(`id`),
+  FOREIGN KEY(`members_id`) REFERENCES `members`(`id`)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- 儲值紀錄
+DROP TABLE IF EXISTS `wallet_record`;
+CREATE TABLE `wallet_record`
+(
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `members_id` INT,
+  `update_wallet` INT,
+  `current_wallet` INT,
+  `desc` VARCHAR(50),
+  `update_time` TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`members_id`) REFERENCES `members`(`id`)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- 點數紀錄
+DROP TABLE IF EXISTS `point_record`;
+CREATE TABLE `point_record`
+(
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `members_id` INT,
+  `update_point` INT,
+  `current_point` INT,
+  `desc` VARCHAR(50),
+  `update_time` TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`members_id`) REFERENCES `members`(`id`)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- 套票
+DROP TABLE IF EXISTS `ticket_set`;
+CREATE TABLE `ticket_set`
+(
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `name` INT,
+  `food_drinks_num` INT,
+  `tickets_num` INT,
+  `desc` VARCHAR(50),
+  `price` TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
