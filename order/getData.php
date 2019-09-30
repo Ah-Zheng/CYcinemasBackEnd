0ab627ec34a67067fb3578509ac23adb16c71e58
Louis3240 <rt4827488@yahoo.com.tw>
2019-09-30 10:05:59 +0800

    add order

---
+++ order/getData.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../header.php';
+require_once '../database.php';
+
+
+switch ($_GET['fields']) {
+    case 'getMovies':
+        $result = $conn -> query("select movies.id,encoded_id,name from movies join movie_time on encoded_id = movies_encoded_id where rating is not null and theaters_name = '國賓影城@台北長春廣場' group by movies.id");
+
+        echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
+        break;
+
+    case 'getMovieDay':
+        $encoded_id = $_GET['encoded_id'];
+
+        $result = $conn -> query("SELECT weekday,date FROM movie_day join movies on movies_encoded_id = encoded_id where movies_encoded_id = '$encoded_id'");
+        echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
+        break;
+
+    case 'getMovieTime':
+        $encoded_id = $_GET['encoded_id'];
+
+        $result = $conn -> query("SELECT time FROM movie_time join movies on movies_encoded_id = encoded_id where movies_encoded_id = '$encoded_id' and theaters_name = '國賓影城@台北長春廣場'");
+        echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
+        break;
+        
+    case 'getTickets':
+        $result = $conn -> query("SELECT name,price FROM tickets");
+        echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
+        break;
+
+    default:
+        break;
+}
+
+mysqli_close($conn);
+
+?>
